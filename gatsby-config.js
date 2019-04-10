ebfe1d97665f252fbd1e458dd2655bc53ea8fa8f
Paul Borm <mail@paulborm.de>
2019-04-10 20:51:52 +0200

    changed meta data, manifest and added deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,38 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    pathPrefix: "/decisionhart",
+    siteMetadata: {
+        title: `DecisionHart`,
+        description: ``,
+        author: `www.paulborm.de`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `decisionHart`,
-        short_name: `decisionHart`,
-        start_url: `/`,
-        background_color: `#8806af`,
-        theme_color: `#8806af`,
-        display: `standalone`,
-        icon: 'src/images/decisionhart-icon.png',
-      },
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-lodash`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `images`,
+            path: `${__dirname}/src/images`,
+        },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+            name: `DecisionHart`,
+            short_name: `DecisionHart`,
+            start_url: `/`,
+            background_color: `#8806af`,
+            theme_color: `#8806af`,
+            display: `standalone`,
+            orientation: `portrait`,
+            icon: `src/images/decisionhart-icon.png`,
+        },
+        },
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-lodash`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        `gatsby-plugin-offline`,
+    ],
 }
