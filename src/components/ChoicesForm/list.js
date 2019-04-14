afe052708d8e66822cab9b8f1ebc55a722a79b02
Paul Borm <mail@paulborm.de>
2019-04-14 20:40:03 +0200

    added in- and out-animation

--- src/components/ChoicesForm/list.js
+++ src/components/ChoicesForm/list.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ChoiceItem from "./ChoiceItem";
 
 import './_list.scss';
@@ -8,24 +9,31 @@ export default function ChoicesList({
         remove
     }) {
 
-    if (items.length <= 0) return null;
-
     const baseName = 'ChoicesList';
 
     return (
         <div className={baseName}>
-            {
-                items.map(item => {
-                    return (
-                        <ChoiceItem
-                            key={item.id}
-                            id={item.id}
-                            title={item.title}
-                            remove={remove}
-                        />
-                    );
-                })
-            }
+            <TransitionGroup>
+                {
+                    items.map(item => {
+                        return (
+                            <CSSTransition
+                                key={item.id}
+                                timeout={250}
+                                classNames={`${baseName}__item`}
+                            >
+                                <div className={`${baseName}__item`}>
+                                    <ChoiceItem
+                                        id={item.id}
+                                        title={item.title}
+                                        remove={remove}
+                                    />
+                                </div>
+                            </CSSTransition>
+                        );
+                    })
+                }
+            </TransitionGroup>
         </div>
     )
 }
