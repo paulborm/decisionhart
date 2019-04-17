4b91c8c868ccb460032a197ac19a6d7e389e0625
Paul Borm <mail@paulborm.de>
2019-04-17 20:52:43 +0200

    transitions
    - added transitions between pages
    - removed transitions on the list items

--- src/components/ChoicesForm/list.js
+++ src/components/ChoicesForm/list.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ChoiceItem from "./ChoiceItem";
-
-import './_list.scss';
 
 export default function ChoicesList({
         items,
@@ -13,27 +10,18 @@ export default function ChoicesList({
 
     return (
         <div className={baseName}>
-            <TransitionGroup>
-                {
-                    items.map(item => {
-                        return (
-                            <CSSTransition
-                                key={item.id}
-                                timeout={250}
-                                classNames={`${baseName}__item`}
-                            >
-                                <div className={`${baseName}__item`}>
-                                    <ChoiceItem
-                                        id={item.id}
-                                        title={item.title}
-                                        remove={remove}
-                                    />
-                                </div>
-                            </CSSTransition>
-                        );
-                    })
-                }
-            </TransitionGroup>
+            {
+                items.map(item => {
+                    return (
+                        <ChoiceItem
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            remove={remove}
+                        />
+                    );
+                })
+            }
         </div>
     )
 }
